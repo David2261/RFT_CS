cccbe9b3f028e42a3ca0e8e87ff5820700601996
David2261 <bulatnasirov2003@gmail.com>
2022-08-31 13:31:54 +0300

    Add file flight_simulation, main_goal and functions of resistance force and body area

--- RFTCS/format.py
+++ RFTCS/format.py
@@ -5,18 +5,19 @@
 
 class RocketFormat:
 	def _rocket_total_speed(num: float) -> str:
-		return f"Сумма всех скоростей = {str(num)} м/с\n"
+		return f"Сумма всех скоростей = {str(num)} м/с"
 
 	def _rocket_natural_logarithm(num: float) -> str:
-		return f"Натуральный логарифм = {str(num)}\n"
+		return f"Натуральный логарифм = {str(num)}"
 
 	def _rocket_massa_construction(num: float) -> str:
-		return f"Сумма всей конструкции ракеты = {str(num)} кг\n"
+		return f"Сумма всей конструкции ракеты = {str(num)} кг"
 
 	def _rocket_total_oil(num: float) -> str:
-		return f"Сумма всего топлива = {str(num)} кг\n"
+		return f"Сумма всего топлива = {str(num)} кг"
 
 
 class FlightFormat:
-	pass
+	def _flight_resistance_force(num: float) -> str:
+		return f"Сила сопротивления = {num} м/с^2"
 
---
+++ RFTCS/rocket_flight_simulation.py
@@ -0,0 +1,37 @@
+from rocket_fuel_calculation import total_speed
+from format import FlightFormat
+
+# плотность воздуха, кг/м^3
+AD = 1
+# молярная масса, кг/моль
+MM = 0.029
+# температура, K
+T = 300
+# Ускорение свободного падения
+GO = 9.81
+# Ро - плотность
+RHO = 16900
+# Коэфициент формы
+Cf = 1.2
+
+
+
+# Функция расчета площади
+def _body_area(height: float, width: float) -> float:
+	total = height * width
+	return total
+
+
+def resistance_force(height: float, width: float) -> float:
+	area = _body_area(height, width)
+	dealta_V = total_speed(420000.0, 5790000.0, 524.0)
+	F = Cf * (RHO * dealta_V**2) / 2 * area
+	return F
+
+
+if __name__ == "__main__":
+	res = resistance_force(100.0, 30.0)
+	print(FlightFormat._flight_resistance_force(res))
+
+# Натуральный логарифм = 9.310151165228136
+# Сила сопротивления = 4.47619780864866e+22 м/с^2
