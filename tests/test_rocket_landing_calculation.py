1ab1c54852b82b4909744fcff5d2ddc2ba7d1081
David2261 <bulatnasirov2003@gmail.com>
2022-09-08 21:06:41 +0300

    Add tests for vector speed function from rlc file

--- tests/test_rocket_landing_calculation.py
+++ tests/test_rocket_landing_calculation.py
@@ -18,7 +18,7 @@ from rocket_landing_calculation import (
 # __new __.__ defaults__ для создания объектов Task без указания всех полей.
 
 
-def data_crm():
+def data_rlc():
 	speed = 528.0
 	resistance = 12.3
 	radius = 87.0
@@ -28,10 +28,11 @@ def data_crm():
 	array = (speed, resistance, radius, elevation, mass, teta)
 	return array
 
+
 @pytest.mark.crm
 class TestCRM:
 	"""Тест с calculation_rocket_movement"""
-	data = data_crm()
+	data = data_rlc()
 	speed = data[0]
 	resistance = data[1]
 
@@ -40,10 +41,6 @@ class TestCRM:
 		tr = calculation_rocket_movement(self.speed, self.resistance)
 		# assertEqual a==b
 		assert tr == result
-
-	def test_calculation_rocket_movement_fail(self):
-		with pytest.raises(TypeError):
-			calculation_rocket_movement('(17,', self.resistance)
 
 	def test_calculation_rocket_movement_type(self):
 		tr = calculation_rocket_movement(self.speed, self.resistance)
@@ -60,8 +57,8 @@ class TestCRM:
 @pytest.mark.crm
 @pytest.mark.exception
 class TestCRMError:
-	"""Тест calculation_rocket_movement с faile"""
-	data = data_crm()
+	"""Тест calculation_rocket_movement с ошибкой"""
+	data = data_rlc()
 	speed = data[0]
 	resistance = data[1]
 
@@ -69,6 +66,54 @@ class TestCRMError:
 		with pytest.raises(TypeError):
 			calculation_rocket_movement('(17,', self.resistance)
 
-	def test_calculation_rocket_movement_fail(self):
+	def test_calculation_rocket_movement_more_args(self):
 		with pytest.raises(TypeError):
 			calculation_rocket_movement(self.speed, self.resistance, 54.2)
+
+	def test_calculation_rocket_movement_less_args(self):
+		with pytest.raises(TypeError):
+			calculation_rocket_movement(self.speed)
+
+
+@pytest.mark.vs
+class TestVS:
+	"""Тест vector_speed"""
+	data = data_rlc()
+	radius = data[2]
+	elev = data[3]
+
+	def test_vector_speed(self):
+		result = vector_speed(self.radius, self.elev)
+		rt = 9.81 * (self.radius ** 2 / ((self.radius + self.elev) ** 2))
+		assert result == rt
+
+	def test_vector_speed_isinstance(self):
+		res_test = vector_speed(self.radius, self.elev)
+		assert isinstance(res_test, (int, float))
+
+	def test_vector_speed_less(self):
+		result = vector_speed(self.radius, self.elev)
+		rt = 9.81 * (self.radius ** 2 / ((self.radius + self.elev) ** 2))
+		rt += 1
+		assert result <= rt
+
+
+@pytest.mark.vs
+@pytest.mark.exception
+class TestVSError:
+	"""Тест vector_speed с ошибкой"""
+	data = data_rlc()
+	radius = data[2]
+	elev = data[3]
+
+	def test_vector_speed_type_error(self):
+		with pytest.raises(TypeError):
+			vector_speed('(17,', self.elev)
+
+	def test_vector_speed_more_args(self):
+		with pytest.raises(TypeError):
+			vector_speed(self.radius, self.elev, 13.2)
+
+	def test_vector_speed_less_args(self):
+		with pytest.raises(TypeError):
+			vector_speed(self.radius)
