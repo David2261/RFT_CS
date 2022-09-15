b00e1e2d5acf042f6103c090b6505b6fad100c4c
David2261 <bulatnasirov2003@gmail.com>
2022-09-15 23:58:50 +0300

    update dir

--- RFTCS/Charts/chart_rfc.py
+++ RFTCS/Charts/chart_rfc.py
@@ -6,17 +6,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def fuel_charts():
 
-def charts() -> None:
-	# evenly sampled time at 200ms intervals
-	t = np.arange(0., 5., 0.2)
-
-	# red dashes, blue squares and green triangles
-	plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
-	plt.show()
 
 
 if __name__ == "__main__":
-	charts()
+	fuel_charts()
 
 
