bf5774988755a317381e8f0ff8c839e686d86809
David2261 <bulatnasirov2003@gmail.com>
2022-09-16 14:14:04 +0300

    Add line chart for fuel rocket

--- RFTCS/Charts/chart_rfc.py
+++ RFTCS/Charts/chart_rfc.py
@@ -6,11 +6,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def fuel_charts():
-
+def fuel_charts(stage: int, y: list) -> None:
+	x = list()
+	for i in range(stage + 1):
+		x.append(i)
+	plt.plot(x, y, label='Скорость')
+	plt.xlabel(
+		'Ступень',
+		fontweight='book',
+		fontstyle='italic',
+		color='blue',
+		fontsize='medium')
+	plt.ylabel(
+		'Скорость',
+		fontweight='book',
+		fontstyle='oblique',
+		color='green',
+		fontsize='medium'
+	)
+	plt.title('Изменение скорости ракеты', size='20')
+	plt.legend()
+	plt.grid(True)
+	plt.show()
 
 
 if __name__ == "__main__":
-	fuel_charts()
+	y_list = [0, 6461.95, 8489.82, 6764.82]
+	fuel_charts(3, y_list)
 
 
