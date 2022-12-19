99f8f2e0ab7b1a903f5d9490910cb23dc2b08267
David2261 <bulatnasirov2003@gmail.com>
2022-12-19 15:47:33 +0300

    Add 50% to logging in to rfs file

--- RFTCS/main.py
+++ RFTCS/main.py
@@ -2,10 +2,10 @@ from multiprocessing.sharedctypes import Value
 import os
 import sys
 import logging
-import traceback
 import logging.config
 
 from exceptions.exception import *
+from setup.logging_conf import LOGGING_CONF
 
 # Логгирование
 # logging.config.fileConfig('log.conf')
@@ -17,45 +17,6 @@ from exceptions.exception import *
 # 	format='%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s((%lineno)d) - %(message)s')
 
 DEBUG = True
-
-LOGGING_CONF = {
-    "disable_existing_loggers": False,
-    "version": 1,
-    "formatters": {
-        "standart": {
-            "format": "%(asctime)s - %(filename)s - %(name)s - %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-        "exception": {
-            "format": "%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s((%lineno)d) - %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        }
-    },
-    "handlers": {
-        "file": {
-            "level": "INFO",
-            "class": "logging.FileHandler",
-            "formatter": "standart",
-			"filename": '__logs__/main.log',
-        },
-        "dev_file": {
-            "level": "NOTSET",
-            "class": "logging.FileHandler",
-            "formatter": "exception",
-			"filename": '__logs__/error.log',
-        },
-    },
-    "loggers": {
-        "root": {
-            "level": "INFO",
-            "handlers": ["file"],
-        },
-        "dev": {
-            "level": "ERROR",
-            "handlers": ["dev_file"],
-        },
-    },
-}
 
 logging.config.dictConfig(LOGGING_CONF)
 logger = logging.getLogger("dev")
--- RFTCS/rocket_flight_simulation.py
+++ RFTCS/rocket_flight_simulation.py
@@ -7,31 +7,40 @@
 	- Общая скорость
 """
 import sys
-import logging
 import numpy as np
+import logging
+import logging.config
 
-from setup.constant import (
-	ATMOSPHERIC_PRESSURE,
-	ATMOSPHERIC_DENSITY,
-	ACCELERATION_FREE_FALL,
-	CROSS_SECTION_AREA,
-	VOLUME_EMPTY_CC,
-	UNIVERSAL_GAS_CONSTANT,
-	EARTH_RADIUS
-)
-from setup.settings import (
-	INITIAL_DISTANCE,
-	FUEL_DENSITY,
-	AVERAGE_MOLAR_MASS,
-	BURNING_TEMPERATURE,
-	FPV,
-	TVV,
-	BEGIN_RADIUS_ROCKET
-)
-from exceptions import logger
+from exceptions.exception import *
+from setup.logging_conf import LOGGING_CONF
 
+logging.config.dictConfig(LOGGING_CONF)
+logger = logging.getLogger("dev")
+log_info = logging.getLogger("root")
 
-log = logger.get_logger(__name__)
+try:
+	from setup.constant import (
+		ATMOSPHERIC_PRESSURE,
+		ATMOSPHERIC_DENSITY,
+		ACCELERATION_FREE_FALL,
+		CROSS_SECTION_AREA,
+		VOLUME_EMPTY_CC,
+		UNIVERSAL_GAS_CONSTANT,
+		EARTH_RADIUS
+	)
+	from setup.settings import (
+		INITIAL_DISTANCE,
+		FUEL_DENSITY,
+		AVERAGE_MOLAR_MASS,
+		BURNING_TEMPERATURE,
+		FPV,
+		TVV,
+		BEGIN_RADIUS_ROCKET
+	)
+	log_info("Включение импортов 'rocket_flight_simulation.py'")
+except ImportError as e:
+	logger.error(invalid_import(e))
+	sys.exit(1)
 
 
 """ Расстояние от горящей поверхности топлива до стенки камеры сгорания. """
@@ -39,64 +48,113 @@ def distance_N_step(U: int, n: int) -> float:
 	try:
 		r0 = INITIAL_DISTANCE
 		r_n = r0 - n * U
-		return float(r_n)
+		log_info.info("Запуск функции 'distance_N_step'")
 	except Exception as e:
-		log.error(e)
+		logger.error(e)
 		sys.exit(1)
+	return float(r_n)
+
 
 
 def tg_Beta(speed: int):
-	R = EARTH_RADIUS
-	v = (speed ** 2 * BEGIN_RADIUS_ROCKET) / 398_621
-	tg_Beta = v / (2 * np.sqrt(1-v))
+	try:
+		log_info.info("Запуск функции 'tg_Beta'")
+		R = EARTH_RADIUS
+		v = (speed ** 2 * BEGIN_RADIUS_ROCKET) / 398_621
+		tg_Beta = v / (2 * np.sqrt(1-v))
+	except Exception as e:
+		logger.error(e)
+		sys.exit(1)
 	return tg_Beta
 
 """ Эллиптическая дальность полета """
 def elliptical_range(speed: int) -> float:
-	R = EARTH_RADIUS
-	L = 2 * R * np.arctan(tg_Beta)
+	try:
+		log_info.info("Запуск функции 'elliptical_range'")
+		R = EARTH_RADIUS
+		L = 2 * R * np.arctan(tg_Beta)
+	except Exception as e:
+		logger.error(e)
+		sys.exit(1)
 	return L
 
 
 """ Масса всей ракеты """
 def mass_rocket(m_empty_rocket: int, m_fuel_rocket: int) -> float:
-	return float(m_empty_rocket + m_fuel_rocket)
+	try:
+		log_info.info("Запуск функции 'mass_rocket'")
+		res = float(m_empty_rocket + m_fuel_rocket)
+	except Exception as e:
+		logger.error(e)
+		sys.exit(1)
+	return res
 
 
 """ Количество выделяемого газа за 1 моль """
 def amount_gas_released(mass: int) -> float:
-	amm = AVERAGE_MOLAR_MASS
-	return float(mass / amm)
+	try:
+		log_info.info("Запуск функции 'amount_gas_released'")
+		amm = AVERAGE_MOLAR_MASS
+		res = float(mass / amm)
+	except Exception as e:
+		logger.error(e)
+		sys.exit(1)
+	return res
 
 
 """ Избыточное давление в камере сгорания на n-шаге """
 def overpressure(U) -> float:
-	amm = AVERAGE_MOLAR_MASS
-	ugc = UNIVERSAL_GAS_CONSTANT
-	bt = BURNING_TEMPERATURE
-	h = amm * ugc * bt
-	return float(h / U)
+	try:
+		log_info.info("Запуск функции 'overpressure'")
+		amm = AVERAGE_MOLAR_MASS
+		ugc = UNIVERSAL_GAS_CONSTANT
+		bt = BURNING_TEMPERATURE
+		h = amm * ugc * bt
+		res =  float(h / U)
+	except Exception as e:
+		logger.error(e)
+		sys.exit(1)
+	return res
 
 
 """ Сила выталкивания (тяги) газов через сопло """
 def thrust_force(P_n: float):
-	csa = CROSS_SECTION_AREA
-	return P_n * csa
+	try:
+		log_info.info("Запуск функции 'thrust_force'")
+		csa = CROSS_SECTION_AREA
+		res = P_n * csa
+	except Exception as e:
+		logger.error(e)
+		sys.exit(1)
+	return res
 
 
 """ Импульс, сообщаемого ракете на n-шаге """
 def impuls(P_n, time):
-	tf = thrust_force(P_n)
-	return float(tf * time)
+	try:
+		log_info.info("Запуск функции 'impuls'")
+		tf = thrust_force(P_n)
+		res =  float(tf * time)
+	except Exception as e:
+		logger.error(e)
+		sys.exit(1)
+	return res
 
 
 """ Высота ракеты над стартовой площадкой """
 def height_rocket(h_n, u, t):
-	return float(h_n + u * t)
+	try:
+		log_info.info("Запуск функции 'height_rocket'")
+		res = float(h_n + u * t)
+	except Exception as e:
+		logger.error(e)
+		sys.exit(1)
+	return res
 
 
 class CylindricalCavity:
 	""" Расчет объема внутренней цилиндрической полости """
+	log_info.info("Запуск класса 'CylindricalCavity'")
 	def __init__(self, speed_burning_fuel: int, step: int, Long: int):
 		self.U = speed_burning_fuel
 		self.n = step
@@ -105,8 +163,13 @@ class CylindricalCavity:
 	@classmethod
 	# Радиус внутренней цилиндрической полости
 	def _cylindrical_cavity(cls) -> float:
-		R_0 = INITIAL_DISTANCE
-		R_n = R_0 + cls.n * cls.U
+		try:
+			log_info.info("Запуск функции 'height_rocket'")
+			R_0 = INITIAL_DISTANCE
+			R_n = R_0 + cls.n * cls.U
+		except Exception as e:
+			logger.error(e)
+			sys.exit(1)
 		return R_n
 
 	# Объем цилиндрической полости
