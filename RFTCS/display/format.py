bf620eaddc6852146cc80ea1e4822b17d157ce47
David2261 <bulatnasirov2003@gmail.com>
2022-12-20 19:24:05 +0300

    Complete refactor in format file

--- RFTCS/display/format.py
+++ RFTCS/display/format.py
@@ -9,117 +9,121 @@ init()
 from colorama import Fore, Back, Style
 
 
-def checker(number):
-	if isinstance(number, (float, int)):
-		return number
-	else: 
-		ValueError()
-
 
 
 class RocketFormat:
-	@checker
-	def _rocket_total_speed(num) -> str:
-		text = f"Сумма всех скоростей = {str(num)} м/с"
+	def __init__(self, num):
+		self.num = num
+
+	def rocket_total_speed(self) -> str:
+		text = f"Сумма всех скоростей = {str(self.num)} м/с"
 		print(Fore.GREEN + text)
 		return text
 
-	@checker
-	def _rocket_natural_logarithm(num: Union[int, float]) -> str:
-		text = f"Натуральный логарифм = {str(num)}"
+	def _rocket_natural_logarithm(self) -> str:
+		text = f"Натуральный логарифм = {str(self.num)}"
 		print(Fore.GREEN + text)
 		return text
 
-	@checker
-	def _rocket_massa_construction(num: Union[int, float]) -> str:
-		text = f"Сумма всей конструкции ракеты = {str(num)} кг"
+	def _rocket_massa_construction(self) -> str:
+		text = f"Сумма всей конструкции ракеты = {str(self.num)} кг"
 		print(Fore.GREEN + text)
 		return text
 
-	@checker
-	def _rocket_total_oil(num: Union[int, float]) -> str:
-		text = f"Сумма всего топлива = {str(num)} кг"
+	def _rocket_total_oil(self) -> str:
+		text = f"Сумма всего топлива = {str(self.num)} кг"
 		print(Fore.GREEN + text)
 		return text
 
 
 class FlightFormat:
-	@checker
-	def _flight_resistance_force(num: float) -> str:
-		text = f"Сила сопротивления = {str(num)} H"
+	def __init__(self, num):
+		self.num = num
+
+	def _flight_resistance_force(self) -> str:
+		text = f"Сила сопротивления = {str(self.num)} H"
 		print(Fore.GREEN + text)
 		return text
 
-	@checker
-	def _flight_resistance_force_env(num: float) -> str:
-		text = f"Сила сопротивления среды = {str(num)} H"
+	def _flight_resistance_force_env(self) -> str:
+		text = f"Сила сопротивления среды = {str(self.num)} H"
 		print(Fore.GREEN + text)
 		return text
 
-	@checker
-	def _flight_distance(num: float) -> str:
-		text = f"Расстояние полета ракеты = {str(num)} км"
+	def _flight_distance(self) -> str:
+		text = f"Расстояние полета ракеты = {str(self.num)} км"
 		print(Fore.GREEN + text)
 		return text
 
-	@checker
-	def _flight_mass(num: float) -> str:
-		text = f"Масса ракеты = {str(num)} т"
+	def _flight_mass(self) -> str:
+		text = f"Масса ракеты = {str(self.num)} т"
 		print(Fore.GREEN + text)
 		return text
 
 
 class LandingFormat:
-	@checker
-	def _calculation_rocket_movement(num: float) -> str:
-		text = f"Математическая модель полета ракеты = {num}"
+	def __init__(self, num):
+		self.num = num
+
+	def _calculation_rocket_movement(self) -> str:
+		text = f"Математическая модель полета ракеты = {self.num}"
 		print(Fore.GREEN + text)
 		return text
 
-	@checker
-	def _vector_speed(num: float) -> str:
-		text = f"Вектор скорости = {num} км/c"
+	def _vector_speed(self) -> str:
+		text = f"Вектор скорости = {self.num} км/c"
 		print(Fore.GREEN + text)
 		return text
 
-	@checker
-	def _rocket_flight_y(num: float) -> str:
-		text = f"Расстояние полета ракеты = {num}"
+	def _rocket_flight_y(self) -> str:
+		text = f"Расстояние полета ракеты = {self.num}"
 		print(Fore.GREEN + text)
 		return text
 
-	@checker
-	def _rocket_flight_x(num: float) -> str:
-		text = f"Угол поворота ракеты tg Beta = {num}"
+	def _rocket_flight_x(self) -> str:
+		text = f"Угол поворота ракеты tg Beta = {self.num}"
 		print(Fore.GREEN + text)
 		return text
 
 
 # Функция для вывода результатов, той или иной функции
 def main_rocket_format(num: float, idea: int) -> str:
+	num = str(num)
 	if idea == 1:
-		return RocketFormat._rocket_total_speed(num)
+		rf = RocketFormat(num)
+		a = rf.rocket_total_speed()
+		return a
 	elif idea == 2:
-		return RocketFormat._rocket_natural_logarithm(num)
+		rf = RocketFormat(num)
+		return rf._rocket_natural_logarithm()
 	elif idea == 3:
-		return RocketFormat._rocket_massa_construction(num)
+		rf = RocketFormat(num)
+		return rf._rocket_massa_construction()
 	elif idea == 4:
-		return RocketFormat._rocket_total_oil(num)
+		rf = RocketFormat(num)
+		return rf._rocket_total_oil()
 	elif idea == 5:
-		return FlightFormat._flight_resistance_force(num)
+		ff = FlightFormat(num)
+		return ff._flight_resistance_force()
 	elif idea == 6:
-		return FlightFormat._flight_distance(num)
+		ff = FlightFormat(num)
+		return ff._flight_distance()
 	elif idea == 7:
-		return FlightFormat._flight_mass(num)
+		ff = FlightFormat(num)
+		return ff._flight_mass()
 	elif idea == 8:
-		return LandingFormat._calculation_rocket_movement(num)
+		lf = LandingFormat(num)
+		return lf._calculation_rocket_movement()
 	elif idea == 9:
-		return LandingFormat._vector_speed(num)
+		lf = LandingFormat(num)
+		return lf._vector_speed()
 	elif idea == 10:
-		return LandingFormat._rocket_flight_y(num)
+		lf = LandingFormat(num)
+		return lf._rocket_flight_y()
 	elif idea == 11:
-		return LandingFormat._rocket_flight_x(num)
+		lf = LandingFormat(num)
+		return lf._rocket_flight_x()
 
 
 if __name__ == '__main__':
-	main_rocket_format('sdasd', 1)
+	text = main_rocket_format('sdasd', 1)
