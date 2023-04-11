ab8480b274394fce870812415f0fcadc1c5e22c9
David2261 <bulatnasirov2003@gmail.com>
2023-04-12 00:08:30 +0300

    Complete logging to sql functions

--- RFTCS/dbms/databaseSQL.py
+++ RFTCS/dbms/databaseSQL.py
@@ -43,7 +43,7 @@ class DataBaseSQL:
 	try:
 		sqlConnect = sqlite3.connect(path)
 		cursor = sqlConnect.cursor()
-		log_info.info("Подключение к БД SQL")
+		log_info.info("Подключение DataBaseSQL к БД SQL")
 	except FileNotFoundError:
 		logger.error(invalid_file(ex))
 		raise FileNotFoundError(invalid_file(ex))
@@ -58,7 +58,7 @@ class DataBaseSQL:
 				WHERE type='table' AND name='%s';""" % self.table)
 			res = True if count_calculations.fetchone() \
 				is not None else False
-			log_info.info("Проверка данных в БД SQL")
+			log_info.info(f"Проверка данных в {self.table} БД SQL")
 		except Exception as ex:
 			logger.error(invalid_general(ex))
 			raise invalid_general(ex)
@@ -91,7 +91,7 @@ class DataBaseSQL:
 					oil INTEGER,
 					Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
 				"""
-			log_info.info("Создание таблиц в БД SQL")
+			log_info.info(f"Создание таблиц в БД SQL - {self.table}")
 		except Exception as ex:
 			logger.error(invalid_general(ex))
 			raise invalid_general(ex)
@@ -151,40 +151,61 @@ class ReadSQL:
 	try:
 		sqlConnect = sqlite3.connect(path)
 		cursor = sqlConnect.cursor()
+		log_info.info("Подключение ReadSQL к БД SQL")
+	except FileNotFoundError:
+		logger.error(invalid_file(ex))
+		raise FileNotFoundError(invalid_file(ex))
 	except Exception as e:
-		print(f"Bad request...\n{e}")
+		logger.error(invalid_general(ex))
+		raise invalid_general(ex)
 
 	def read_all_data(self):
-		if self.table == "TotalOil":
-			query = """SELECT * FROM TotalOil;"""
-		elif self.table == "FlightBallistics":
-			query = """SELECT * FROM FlightBallistics;"""
-		elif self.table == "ModelFlight":
-			query = """SELECT * FROM ModelFlight;"""
-		res = self.cursor.execute(query)
-		print(res.fetchall())
+		try:
+			if self.table == "TotalOil":
+				query = """SELECT * FROM TotalOil;"""
+			elif self.table == "FlightBallistics":
+				query = """SELECT * FROM FlightBallistics;"""
+			elif self.table == "ModelFlight":
+				query = """SELECT * FROM ModelFlight;"""
+			res = self.cursor.execute(query)
+			print(res.fetchall())
+			log_info.info(f"Чтение данных из {self.table} в БД SQL")
+		except Exception as ex:
+			logger.error(invalid_general(ex))
+			raise invalid_general(ex)
 		self.cursor.close()
 
 	def read_item_data(self):
-		if self.table == "TotalOil":
-			query = """SELECT * FROM TotalOil WHERE id = ?;"""
-		elif self.table == "FlightBallistics":
-			query = """SELECT * FROM FlightBallistics WHERE id = ?;"""
-		elif self.table == "ModelFlight":
-			query = """SELECT * FROM ModelFlight WHERE id = ?;"""
-		res = self.cursor.execute(query, (self.item, ))
-		print(res.fetchone())
+		try:
+			if self.table == "TotalOil":
+				query = """SELECT * FROM TotalOil WHERE id = ?;"""
+			elif self.table == "FlightBallistics":
+				query = """SELECT * FROM FlightBallistics WHERE id = ?;"""
+			elif self.table == "ModelFlight":
+				query = """SELECT * FROM ModelFlight WHERE id = ?;"""
+			res = self.cursor.execute(query, (self.item, ))
+			print(res.fetchone())
+		log_info.info(f"Чтение одной записи из {self.table} в БД SQL")
+		except Exception as ex:
+			logger.error(invalid_general(ex))
+			raise invalid_general(ex)
 		self.cursor.close()
 
 	def read_many_data(self):
-		if self.table == "TotalOil":
-			query = """SELECT * FROM TotalOil;"""
-		elif self.table == "FlightBallistics":
-			query = """SELECT * FROM FlightBallistics;"""
-		elif self.table == "ModelFlight":
-			query = """SELECT * FROM ModelFlight;"""
-		res = self.cursor.execute(query)
-		print(res.fetchmany(self.size))
+		try:
+			if self.table == "TotalOil":
+				query = """SELECT * FROM TotalOil;"""
+			elif self.table == "FlightBallistics":
+				query = """SELECT * FROM FlightBallistics;"""
+			elif self.table == "ModelFlight":
+				query = """SELECT * FROM ModelFlight;"""
+			res = self.cursor.execute(query)
+			print(res.fetchmany(self.size))
+			log_info.info(f"Чтение нескольких записей \
+				из {self.table} в БД SQL")
+		except Exception as ex:
+			logger.error(invalid_general(ex))
+			raise invalid_general(ex)
 		self.cursor.close()
 
 
@@ -197,8 +218,13 @@ class PopSQL:
 	try:
 		sqlConnect = sqlite3.connect(path)
 		cursor = sqlConnect.cursor()
+		log_info.info("Подключение PopSQL к БД SQL")
+	except FileNotFoundError:
+		logger.error(invalid_file(ex))
+		raise FileNotFoundError(invalid_file(ex))
 	except Exception as e:
-		print(f"Bad request...\n{e}")
+		logger.error(invalid_general(ex))
+		raise invalid_general(ex)
 
 	def pop_data(self):
 		try:
@@ -209,8 +235,10 @@ class PopSQL:
 			elif self.table == "ModelFlight":
 				query = """DELETE FROM ModelFlight WHERE id = ?;"""
 			self.cursor.execute(query, (self.item, ))
+			log_info.info(f"Удаление одной записи из {self.table} в БД SQL")
 		except Exception as ex:
-			print(f"Не получилось удалить запись...\n{ex}")
+			logger.error(invalid_general(ex))
+			raise invalid_general(ex)
 		self.cursor.close()
 
 	def pop_last_data(self):
@@ -225,8 +253,11 @@ class PopSQL:
 				query = """DELETE FROM ModelFlight
 					WHERE id = (SELECT MAX(id) FROM ModelFlight);"""
 			self.cursor.execute(query)
+			log_info.info(f"Удаление последней записи\
+				из {self.table} в БД SQL")
 		except Exception as ex:
-			print(f"Не получилось удалить запись...\n{ex}")
+			logger.error(invalid_general(ex))
+			raise invalid_general(ex)
 		self.cursor.close()
 
 
