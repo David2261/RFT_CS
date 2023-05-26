20928bc43300bf79d6433545d787ed7d8764b426
David2261 <bulatnasirov2003@gmail.com>
2023-05-26 14:20:28 +0300

    Complete function databaseJson

--- RFTCS/dbms/databaseJSON.py
+++ RFTCS/dbms/databaseJSON.py
@@ -1,7 +1,6 @@
 import json
 from datetime import datetime
 import random
-import numpy as np
 from numpyencoder import NumpyEncoder
 
 
@@ -15,7 +14,7 @@ class JsonLoad:
         if self.name == 'TotalOil':
             json_data = [
                 {
-                    "id": np.random.randint(low=0, high=10, size=(10,)).astype(int),
+                    "id": ''.join([random.choice(list('123456789ABCDEF')) for x in range(12)]),
                     "speed": self.data[0],
                     "oil": self.data[1],
                     "date": datetime.now().strftime('%d.%m.%y | %X')
@@ -24,7 +23,7 @@ class JsonLoad:
         elif self.name == 'FlightBallistics':
             json_data = [
                 {
-                    "id": np.random.randint(low=0, high=10, size=(10,)).astype(int),
+                    "id": ''.join([random.choice(list('123456789ABCDEF')) for x in range(12)]),
                     "flight_range": self.data[0],
                     "flight_time": self.data[1],
                     "date": datetime.now().strftime('%d.%m.%y | %X')
@@ -33,7 +32,7 @@ class JsonLoad:
         elif self.name == 'ModelFlight':
             json_data = [
                 {
-                    "id": np.random.randint(low=0, high=10, size=(10,)).astype(int),
+                    "id": ''.join([random.choice(list('123456789ABCDEF')) for x in range(12)]),
                     "flow": self.data[0],
                     "mass": self.data[1],
                     "speed_0": self.data[2],
@@ -44,7 +43,7 @@ class JsonLoad:
             print("error")
             json_data = [
                 {
-                    "id": np.random.randint(low=0, high=10, size=(10,)).astype(int),
+                    "id": ''.join([random.choice(list('123456789ABCDEF')) for x in range(12)]),
                     "message": "ERROR DATA",
                     "date": datetime.now().strftime('%d.%m.%y | %X')
                 }
@@ -52,7 +51,8 @@ class JsonLoad:
         return json_data
     
     def json_entire(self):
-        data_file = json.load(open('record.json'))
+        with open('record.json', 'r', encoding='utf-8', errors='ignore') as f:
+            data_file = json.loads(f.read())
         json_data = self.generate_data()
         data_file.append(json_data)
         with open('record.json', 'w', encoding='utf-8') as file:
