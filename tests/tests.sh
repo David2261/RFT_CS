96f596ca1835b5cf5c0d4f75c6d6f67616c97e04
David2261 <bulatnasirov2003@gmail.com>
2023-07-20 17:58:29 +0300

    Add documentary about extension CPython API

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 # File for run tests
 
-echo "core(1) | display(2) | setup(3) | main(4)";
+echo "core(1) | display(2) | main(4)";
 read context_id;
 
 if [[ $context_id -eq 1 ]] || [[ $context_id -eq "core" ]]; then
-		pytest core;
+		echo "pytest fails: "
+		pytest rocket_flight_simulation;
+		pytest rocket_flight_trajectory;
+		pytest rocket_fuel_calculation;
 		echo "flake8 fails: "
-		flake8 --count core;
+		flake8 --count rocket_flight_simulation;
+		flake8 --count rocket_flight_trajectory;
+		flake8 --count rocket_fuel_calculation;
 	elif [[ $context_id -eq 2 ]] || [[ $context_id -eq "display" ]]; then
 		pytest display;
 		echo "flake8 fails: "
 		flake8 --count display;
-	elif [[ $context_id -eq 3 ]] || [[ $context_id -eq "setup" ]]; then
-		pytest setup;
-		echo "flake8 fails: "
-		flake8 --count setup;
-	elif [[ $context_id -eq 4 ]] || [[ $context_id -eq "main" ]]; then
+	elif [[ $context_id -eq 3 ]] || [[ $context_id -eq "main" ]]; then
 		pytest test_main.py
 		echo "flake8 fails: "
 		flake8 --count test_main.py
